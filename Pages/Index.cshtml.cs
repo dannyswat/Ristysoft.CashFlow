3cee64d0f35ad24bddd3a2fca32392f8a1d3a261
Dannys Wat <dannys@popularit.com>
2021-06-30 19:01:24 +0800

    Add expense summary

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,30 @@ namespace Ristysoft.CashFlow.Pages
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+		private readonly Ristysoft.CashFlow.Data.ApplicationDbContext _context;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(ILogger<IndexModel> logger,
+			Ristysoft.CashFlow.Data.ApplicationDbContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
-		public void OnGet()
+		public async Task OnGet()
 		{
+			var date = DateTime.Today;
+			var monthStart = new DateTime(date.Year, date.Month, 1);
+			ViewData["ThisMonthExpense"] = (await _context.Expenses.Where(r =>
+				r.Date >= monthStart &&
+				r.Date < monthStart.AddMonths(1))
+				.Select(r => r.Amount).ToArrayAsync())
+				.Sum();
 
+			ViewData["LastMonthExpense"] = (await _context.Expenses.Where(r =>
+				r.Date >= monthStart.AddMonths(-1) &&
+				r.Date < monthStart)
+				.Select(r => r.Amount).ToArrayAsync())
+				.Sum();
 		}
 	}
 }
